790c1a262b11b231de7b9b0cdc0e3bcf32ef0602
Poorva Gupta <poorvagupta352@gmail.com>
2024-11-06 19:01:15 +0530

    Add functionality to return the number itself when a single number is passed

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.new.add("")).to eq(0)
     end
+
+    it 'returns the number itself when a single number is passed' do
+      expect(StringCalculator.new.add("1")).to eq(1)
+    end
   end
 end
 
